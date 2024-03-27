2a76650fc136d4d52c71307aa2edf3233623ecbd
siddharth464 <siddharthpanchal464@gmail.com>
2024-03-27 14:10:19 +0530

    chanes in files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:7000',
-    },
-  },
   plugins: [react()],
 })
